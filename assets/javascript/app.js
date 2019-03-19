445186b031052b85dcd57884bd8943001e5d530a
Rob Conway <robconway12@gmail.com>
2019-03-18 23:14:44 -0700

    created files and started HTML and CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// Declare the variables: countCorrect, countIncorrect
+
+// Title page with image, instructions, and start button.
+// Player Clicks start button to begin.
+
+// Game selects a question at random. The question must be not yet seen.
+// Timer begins counting 1 minute
+
+// If player does not respond before time runs out, the times up function must display the correct answer and display a times out message.
+// After 5 seconds, the game will select a new question.
+
+// If the player gets it right, the "Correct" function will display a congratulations.
+// After 5 seconds, the game will select a new question.
+
+// If the player gets it wrong, the "Incorrect" function will display the correct answer.
+// After 5 seconds, the game will select a new question.
+
+// Once all questions are shown, the score will tally up and display the result of right and wrong answers. It will also have a restart button.
+
+// List question objects
+
+let questions {
+    questionNumber: 
+}
