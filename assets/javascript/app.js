f8486d9e0b6f1e4cbb89942ba65b7ef7c47ce9dc
Rob Conway <robconway12@gmail.com>
2019-03-24 01:43:51 -0700

    fixed more bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // score totals
   var countCorrect = 0;
   var countIncorrect = 0;
-  var time = 31; // timer at 31 secs
+  var time = 32; // timer at 31 secs
   var counter = 0; // question counter
 
   $("#startButton").click(function(){
@@ -27,13 +27,24 @@ $(document).ready(function() {
         "<button  class=\"clickMe\">" + getQuestion[counter].options[3] + "</button>")
     };
 
+  // send to results screen
+  $(document).on('click', '.clickMe', function() { 
+    var playerChoice = $(this).text();
+    if (playerChoice === getQuestion[counter].correctAnswer) {
+      clearInterval(clock);
+      correctGuess();
+    }
+    else {
+      clearInterval(clock);
+      incorrectGuess();
+    };
+  });
+
   // player guessed correctly
   function correctGuess() {
-    $("#choiceBox").html("<p>Yayyy! That's the right answer!</p>");
+    $("#choiceBox").html("<p class='bright'>Yayyy! That's the right answer!</p>");
     countCorrect++;
-    var correctAnswer = getQuestion[counter].correctAnswer;
-    $("#choiceBox").append("<p>The answer was <span class='answer'>" + correctAnswer + "</span></p>");
-    setTimeout(nextQuestion, 4000);
+    setTimeout(nextQuestion, 5000);
     counter++;
   };
 
@@ -42,7 +53,7 @@ $(document).ready(function() {
     $("#choiceBox").html("<p>Sorry! That's incorrect.</p>");
     countIncorrect++;
     var correctAnswer = getQuestion[counter].correctAnswer;
-    $("#choiceBox").append("<p>The answer was <span>" + correctAnswer + "</span></p>");
+    $("#choiceBox").append("<p>The answer was <span class='bright'>" + correctAnswer + "</span></p>");
       setTimeout(nextQuestion, 5000);
     counter++;
   };
@@ -61,12 +72,14 @@ $(document).ready(function() {
   };
 
   // Show result screen
-  $("#choiceBox").html("<p>" + endMessage + "</p>" + "<p>You answered" + 
-    countCorrect + "questions correctly!</p>" + 
-    "<p>You answered" + countIncorrect + "questions incorrectly.</p>");
-  $("#choiceBox").append("<h1 id='start'>Start Over?</h1>");
-  gameReset();
-  $("#start").click(nextQuestion);
+  function resultsScreen() {
+    $("#choiceBox").empty(); 
+    $("#tally").html("<p>You answered <span class='bright'>" + countCorrect + "</span> questions correctly!<br></p>" + 
+      "<p>You answered <span class='bright'>" + countIncorrect + "</span> questions incorrectly.</p>");
+    $("#startbutton").append("<button id=\"start\"></button>");
+    resetQuiz();
+    $("#start").click(nextQuestion);
+  };
 
   // game clock currently set to 31 seconds
   function timer() {
@@ -79,14 +92,14 @@ $(document).ready(function() {
       if (time > 0) {
         time--;
       }
-      $("#timer").text("Time remaining: 00:" + time);
+      $("#timer").html("<p>Time remaining: <span class='bright'>" + time + "</span></p>");
     }
   };
 
   // moves question counter to next question
   function nextQuestion() {
     if (counter < getQuestion.length) {
-      time = 31;
+      time = 32;
       $('#choiceBox').empty();
       populateQuestions();
       timer();
@@ -98,7 +111,7 @@ $(document).ready(function() {
   };
 
   // reset score and counter parameters on restart
-  function gameReset() {
+  function resetQuiz() {
     counter = 0;
     countCorrect = 0;
     countIncorrect = 0;
@@ -111,9 +124,6 @@ $(document).ready(function() {
 				'The first known ice cream recipe was handwritten in the recipe book of Lady Anne Fanshawe in 1665. What did she list as a possible ingredient?',
 			options: ['orange flower water', 'mace', 'whale vomit', 'All of the above'],
 			correctAnswer: 'All of the above',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '2',
@@ -121,27 +131,18 @@ $(document).ready(function() {
 				'Which flavor was considered quite exotic and rare in the late 1700s, as it was difficult to acquire before the mid-19th century?',
 			options: ['strawberry', 'vanilla', 'pistachio', 'chocolate'],
 			correctAnswer: 'Vanilla',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '3',
 			question: 'All of the following flavors were considered popular in Colonial America, except for which one?',
 			options: ['Tomato', 'Asparagus', 'Oyster', 'Parmesan'],
 			correctAnswer: 'Tomato',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '4',
 			question: 'The H&#228;agen-Dazs brand was established in which country?',
 			options: ['Denmark', 'Canada', 'Germany', 'United States'],
 			correctAnswer: 'United States',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '5',
@@ -149,18 +150,12 @@ $(document).ready(function() {
 				"In 1991, Ben & Jerry's invented which flavor based on an anonymous suggestion placed on a board in their Vermont shop?",
 			options: ['Chocolate Chip Cookie Dough', 'Mint Chocolate Chip', 'Cherry Garcia', 'Moose Tracks'],
 			correctAnswer: 'Chocolate Chip Cookie Dough',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '6',
 			question: 'What do food photographers frequently use as a stand-in for actual ice cream in photos?',
 			options: ['Colored Butter', 'Hardened Mousse', 'Modified Mashed Potatoes', 'Molding Clay'],
 			correctAnswer: 'Modified Mashed Potatoes',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '7',
@@ -168,18 +163,12 @@ $(document).ready(function() {
 				"Ben & Jerry's ice cream flavors are full of chunky mix-ins in part because co-founder Ben Cohen gets a lot of his pleasure in ice cream from its texture due to what?",
 			options: ['No sense of taste', 'No sense of smell', 'Lactose intolerant', 'Poor eye sight'],
 			correctAnswer: 'No sense of smell',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '8',
 			question: 'Which popular drink was originally intended as an ice cream topping in its syrup form?',
 			options: ['Cola', 'Tang', 'Hawaiian Punch', 'Root Beer'],
 			correctAnswer: 'Hawaiian Punch',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '9',
@@ -187,32 +176,14 @@ $(document).ready(function() {
 				'Professional ice cream taste-testers use special spoons made of what, which allow the tester to taste the product with virtually no trace of flavor left over from what was last on the spoon?',
 			options: ['Gold', 'Bakelite', 'Silver', 'Glass'],
 			correctAnswer: 'Gold',
-			playerChoice: '',
-			correct: false,
-			time: 0,
 		},
 		{
 			questionNumber: '10',
 			question:
 				'In the West and Texas we are used to Dreyers brand ice cream, but what is it known as in the East and Midwest?',
-			options: ["Edy's", "Breyer's", 'Good Humor', "Cornetto's"],
-			correctAnswer: "Edy's",
-			playerChoice: '',
-			correct: false,
-			time: 0,
+			options: ["Edy\'s", "Breyer\'s", 'Good Humor', "Cornetto\'s"],
+			correctAnswer: "Edy\'s",
 		}
   ];
   
-      // click function to trigger right or wrong screen
-    $("#choiceBox").click(function() {
-      var playerChoice = $(this).text();
-      if (playerChoice === getQuestion[counter].correctAnswer) {
-        clearInterval(clock);
-        correctGuess();
-      }
-      else {
-        clearInterval(clock);
-        incorrectGuess();
-      };
-    });
 });
